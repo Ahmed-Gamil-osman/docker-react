a2f8523134cbbba091862fdc4d1d32e35b53bd28
Ahmed-Gamil-osman <ahmed.gamil@student.aast.edu>
2025-08-26 22:11:53 +0300

    add new tests to be tested using jenkins pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -26,4 +26,17 @@ test('renders learn react link', () => {
 });
 
 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learn from Gamil/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learn from Gamil/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
 
