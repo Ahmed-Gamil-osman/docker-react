aabe9aa2a6c59ff578ba77c9bccab85a8333e7cb
Ahmed-Gamil-osman <ahmed.gamil@student.aast.edu>
2025-08-30 16:15:15 +0300

    kindly check this out

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gamed ya Gamed   
+          Gamed ya Gamed we are now can use the CICD   
         </p>
         <a
           className="App-link"
